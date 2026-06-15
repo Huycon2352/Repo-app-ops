93b33969809b4dd885b4a785b276f54f3824d087
Huycon2352 <lephuchuy951@gmail.com>
2026-06-15 20:08:13 +0700

    add : helm install loki

--- helm-tools/loki/setup-loki.sh
+++ helm-tools/loki/setup-loki.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-helm upgrade --install loki grafana/loki -f loki-values-v2.yaml -n monitoring
+#!/bin/bash
+
+
+
+
+helm repo add grafana https://github.io
+helm repo update
+
+#helm upgrade --install loki grafana/loki -f loki-values-v2.yaml -n monitoring
+
+helm upgrade --install loki-stack grafana/loki-stack \
+  --create-namespace \
+  --namespace logging \
+  --set loki.enabled=true \
+  --set promtail.enabled=true
+
